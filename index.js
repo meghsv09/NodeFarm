d3304f730e489a487ac83eccb33c186dee8e331f
Megha Somaradder <meghasv@Meghas-MacBook-Pro.local>
2024-08-03 15:26:53 +0530

    first commit

--- index.js
+++ index.js
@@ -1,68 +1,48 @@
-"use-strict";
+//NODE FARM APP CODE
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const { log } = require('console');
 
-//Module import
-const fs = require("fs"); //returns object
-const http = require("http");
+//EXPORT YOUR MODULE
+const replaceTemplate = require('./modules/replaceTemplate');
 
-// //Sync way of reading the file, also called blocking code
-// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-// console.log(textIn);
+//READ FILES SYNC'LY
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 
-// const textOut = `This is what we know about avocado: ${textIn}. \n Created on ${Date.now()}. Add text here`;
-// fs.writeFileSync('./txt/output.txt', textOut);
-// console.log('File created and written succssfully');
-
-//Async or Non blovking code
-
-// fs.readFile('./txt/start.txt','utf-8',(err, data) => {
-//     console.log(data);
-// });
-
-// console.log('reading the file');
-
-//Callback hell
-// fs.readFile('./txt/start.txt','utf-8',(err, data1) => {
-//     if(err) return console.log('ERROR');
-//     fs.readFile(`./txt/${data1}.txt`,'utf-8', (err, data2) => {
-//         console.log(data2);
-//         fs.readFile('./txt/append.txt','utf-8',(err, data3) => {
-//             console.log(data3);
-//             fs.writeFile('./txt/final.txt',`${data2}\n ${data3}`,'utf-8',err => {
-//                 console.log('File written successfully!');
-//             });
-//         });
-//     });
-// });
-
-// const server = http.createServer((req, res) => {
-//     res.end('Hello World!');
-// });
-
-// server.listen(8000, '127.0.0.1', () => {
-//     console.log('Listening to reqests on port 8000');
-// });
-
-//Read JSON file and parse it
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
-//Routing
+//SERVER
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
-  if (pathName === "/" || pathName === "/overview") {
-    res.end("This is Overview");
-  } else if (pathName === "/product") {
-    res.end("This is Product");
-  } else if (pathName === "/api") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(data);
-  } else {
-    res.writeHead(404, "Not found the page", { "Content-type": "text/html" });
-    res.end("<h1>Page not found</h1>");
+  const { query, pathname } = url.parse(req.url, true);
+
+  //OVERVIEW PAGE
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el)).join('');
+    // console.log(cardsHtml);
+    const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
+    res.end(output);
   }
-  // console.log(__dirname);
+
+  //PRODUCT PAGE
+  else if (pathname == '/product') {
+    const product = dataObj[query.id];
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+  }
+
+  //ANY OTHER PAGE
+  else {
+    res.writeHead(200, 'Not Found', { 'Content-type': 'text/html' });
+    res.end('<h1>Page not found!</h1>');
+  }
 });
 
-server.listen(8000, "127.0.0.1", () => {
-  console.log("Listening to reqests on port 8000");
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to port 8000');
 });
